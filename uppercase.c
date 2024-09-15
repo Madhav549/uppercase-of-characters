95222fc47e45397c5f6b688ba8ca6506754bf727
Madhav Varshney <madhav.02varshney@gmail.com>
2024-09-15 22:09:47 +0530

    to check the uppercase letters are present or not

--- uppercase.c
+++ uppercase.c
@@ -1,12 +1,12 @@
-// #include<stdio.h>
-// int main(){
-// char ch;
-//   printf("enter a character:");
-//   scanf("%c",&ch);
-// if(ch>='A' && ch<='Z'){
-//   printf("It is UPPERCASE");
-// }
-// else
-//   printf("It is LOWERCASE");
-//   return 0;
-// }
+   #include<stdio.h>
+   int main(){
+   char ch;
+     printf("enter a character:");
+     scanf("%c",&ch);
+   if(ch>='A' && ch<='Z'){
+     printf("It is UPPERCASE");
+   }
+   else
+     printf("It is LOWERCASE");
+     return 0;
+   }
